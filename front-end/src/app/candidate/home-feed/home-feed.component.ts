c6917cf98c92062f414e1e681b65ae05d7109ad2
Anas <alikhanm@taffinc.com>
2024-09-25 13:36:00 +0530

    fixed

--- front-end/src/app/candidate/home-feed/home-feed.component.ts
+++ front-end/src/app/candidate/home-feed/home-feed.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home-feed',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatDividerModule, MatListModule],
+  imports: [MatButtonModule, MatCardModule, MatDividerModule, MatListModule,CommonModule],
   templateUrl: './home-feed.component.html',
   styleUrl: './home-feed.component.scss'
 })
 export class HomeFeedComponent {
+  isExpanded: boolean = false;
 
+  // Sample paragraph
+  paragraph: string = `This is a sample paragraph. You can write some content here and when the content exceeds 
+  a certain length, you can click.`;
+
+  toggleReadMore() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
