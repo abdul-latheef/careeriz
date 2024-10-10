3f23a5c3ed89bbd8766312cfdd09c70f92b6ac7d
latheef <abdul.latheef61@gmail.com>
2024-10-10 12:43:40 +0530

    fixed some ui

--- front-end/src/app/candidate/dashboard/dashboard.component.ts
+++ front-end/src/app/candidate/dashboard/dashboard.component.ts
@@ -3,10 +3,11 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { HomeFeedComponent } from '../home-feed/home-feed.component';
 import { ResumeBuildComponent } from '../resume-build/resume-build.component';
 import { ResumeBuildDetailsComponent } from "../resume-build-details/resume-build-details.component";
+import { MatCardModule } from '@angular/material/card'
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatTabsModule, HomeFeedComponent, ResumeBuildComponent, ResumeBuildDetailsComponent],
+  imports: [MatTabsModule, HomeFeedComponent, ResumeBuildComponent, ResumeBuildDetailsComponent, MatCardModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
